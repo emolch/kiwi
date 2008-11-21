5b3f18b15c50ee7ec01ab40db5c38a794af2f5e7
Sebastian Heimann <sebastian.heimann@zmaw.de>
2008-11-21 10:50:24 +0000

    added some comments to Makefile.local.example

--- export-packages.sh
+++ export-packages.sh
@@ -5,58 +5,20 @@ repos=$SSS
 rm -rf /tmp/kiwi_export
 mkdir /tmp/kiwi_export
 cd /tmp/kiwi_export
-svn export $repos/kinherd/kiwi/trunk kiwi > exportlog || exit 1
+svn export $repos/kiwi/trunk kiwi > exportlog || exit 1
 tail -n 1 exportlog > kiwi/VERSION
 date >> kiwi/VERSION
 version=`cat exportlog | tail -n 1 | tr -d -c '[0-9]'`
 svn log $repos/kinherd/kiwi/trunk > kiwi/CHANGES
 mv kiwi kiwi-r$version
 tar -czvf kiwi-r$version.tar.gz kiwi-r$version
-mv kiwi-r$version kiwi
-cd kiwi
-machine=`./hostinfo.pl --machine`
-os=`./hostinfo.pl --os`
-
-cat > Makefile.local <<EOF
-FORTRANC = g95
-CFLAGS += \$(CFAST)
-EOF
-
-make || exit 1
-
-cat > bin/README <<EOF
-This directory contains the Kinherd Kiwi programs.
-
-Compiled for:
-  $os $machine
-
-Documentation:
-  http://kinherd.org
-
-Compilation host:
-  $uname_a
-
-Revision:
-  r$version 
-EOF
-cp VERSION bin/
-cp CHANGES bin/
-
-cp LICENSE bin/
-cp NOTICE bin/
-
-vom=r$version-$os-$machine
-cp -R bin kiwi-$vom-bin
-tar -czvf kiwi-$vom-bin.tar.gz kiwi-$vom-bin
-mv kiwi-$vom-bin.tar.gz ..
-cd ..
 
 echo "Everything OK? Shall I copy the packages to the server? [y/N]"
 read answer
 
 if [ $answer == 'y' ]; then
-    scp kiwi-r$version.tar.gz kiwi-$vom-bin.tar.gz emolch:www/kinherd/software || exit 1
-    echo "removing build files..."
+    scp kiwi-r$version.tar.gz emolch:www/kinherd/software/ || exit 1
+    echo "removing exported files..."
     rm -rf /tmp/kiwi_export
 else 
     echo "I will leave the files in /tmp/kiwi_export"
