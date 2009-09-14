8d8afbfadc03071362da09ea283f3480996d4140
Sebastian Heimann <sebastian.heimann@zmaw.de>
2009-09-14 08:52:07 +0000

    added non-functional dummy sacio lib to not depend on libsacio which is not available on 64 bit systems.

---
+++ dummy_sacio/sacio.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void wsac1_( char *filename, float *seismogram, int *nlen, 
+            float *toffset, float *deltat, int *nerr ) {
+    fprintf(stderr, "Non-functional dummy function wsac1() called. Exiting...\n");
+    exit(1);
+}
+
+void rsac1_( char *filename, float *scratch, int *nlen, 
+            float *toffset, float *deltat, int *maxlen, int *nerr) {
+    fprintf(stderr, "Non-functional dummy function rsac1() called. Exiting...\n");
+    exit(1);
+}
