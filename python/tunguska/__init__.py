1c1f81edd9a50da42f1c21879c55b719822418ea
Sebastian Heimann <sebastian.heimann@zmaw.de>
2009-11-08 19:52:29 +0000

    quick!

--- python/tunguska/__init__.py
+++ python/tunguska/__init__.py
@@ -5,8 +5,9 @@ from tunguska.config import Config
 from tunguska.inversion import (Informer, WeightMaker,EffectiveDtTester, ParamTuner, Greeper,
     Shifter, EnduringPointSource, TracePlotter)
 from tunguska.main import kiwi_main
+import tunguska.config as global_config
 
 __all__ = ['Taper', 'Filter', 'Config', 'Informer',
            'WeightMaker', 'EffectiveDtTester', 'ParamTuner', 'Greeper', 'Shifter', 'EnduringPointSource',
-           'TracePlotter', 'kiwi_main']
+           'TracePlotter', 'kiwi_main', 'global_config']
 
