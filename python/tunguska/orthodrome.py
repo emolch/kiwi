9b58ce307c804cd1aad549aa18192a54edf5898a
Sebastian Heimann <sebastian.heimann@zmaw.de>
2009-11-24 13:05:52 +0000

    added python versions of azimuth and dist calculations to orthodrome module

--- python/tunguska/orthodrome.py
+++ python/tunguska/orthodrome.py
@@ -5,12 +5,51 @@ import config
 
 d2r = math.pi/180.
 r2d = 1./d2r
+earth_oblateness = 1./298.257223563
+earthradius_equator = 6378.14 * 1000.
 
 def clip(x, mi, ma):
     return num.minimum(num.maximum(mi,x),ma)
         
 def wrap(x, mi, ma):
     return x - num.floor((x-mi)/(ma-mi)) * (ma-mi)
+
+def cosdelta( a, b ):
+    return math.sin(a.lat*d2r) * math.sin(b.lat*d2r) + math.cos(a.lat*d2r) * math.cos(b.lat*d2r) * math.cos(d2r*(b.lon-a.lon))
+
+def azimuth( a, b ):
+    return r2d*math.atan2( math.cos(a.lat*d2r) * math.cos(b.lat*d2r) * math.sin(d2r*(b.lon-a.lon)),
+                           math.sin(d2r*b.lat) - math.sin(d2r*a.lat) * cosdelta(a,b) )
+
+def distance_accurate50m( a, b ):
+
+    # more accurate distance calculation based on a spheroid of rotation
+
+    # returns distance in [m] between points a and b
+    # coordinates must be given in degrees
+
+    # should be accurate to 50 m using WGS84
+    
+    # from wikipedia :  http://de.wikipedia.org/wiki/Orthodrome
+    # based on: Meeus, J.: Astronomical Algorithms, S 85, Willmann-Bell,
+    #           Richmond 2000 (2nd ed., 2nd printing), ISBN 0-943396-61-1
+    
+    
+    f = (a.lat + b.lat)*d2r / 2.
+    g = (a.lat - b.lat)*d2r / 2.
+    l = (a.lon - b.lon)*d2r / 2.
+
+    s = math.sin(g)**2 * math.cos(l)**2 + math.cos(f)**2 * math.sin(l)**2
+    c = math.cos(g)**2 * math.cos(l)**2 + math.sin(f)**2 * math.sin(l)**2
+
+    w = math.atan( math.sqrt( s/c ) )
+    r = math.sqrt(s*c)/w
+    d = 2.*w*earthradius_equator
+    h1 = (3.*r-1.)/(2.*c)
+    h2 = (3.*r+1.)/(2.*s)
+
+    return d * (1.+ earth_oblateness * h1 * math.sin(f)**2 * math.cos(g)**2 - 
+                    earth_oblateness * h2 * math.cos(f)**2 * math.sin(g)**2)
 
 def ne_to_latlon( lat0, lon0, north_m, east_m ):
     
