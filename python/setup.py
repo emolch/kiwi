3f354547b0d4a03f629d53da66296d344489af32
Sebastian Heimann <sebastian.heimann@zmaw.de>
2011-03-01 11:04:02 +0000

    further cleanup

--- python/setup.py
+++ python/setup.py
@@ -1,16 +1,25 @@
 
 from distutils.core import setup
 
-setup( name    = 'kiwi',
-       version = '0.1',
-       description = 'Python interface to the Kiwi codes.',
-       py_modules = ['ugly_minimizer', 'QVTKRenderWindowInteractor', 'pysacio'],
-       packages = ['tunguska'],
-       scripts = ['kinherd_sourceview',
-                  'kinherd_sourceview_pres', 'gfdb_phaser', 'gfdb_scale',
-                   'autokiwi'],
-       author = 'Sebastian Heimann',
-       author_email = 'sebastian.heimann@zmaw.de',
-       url = 'http://www.kinherd.org/',
-       )
+from os.path import join as pjoin
+
+setup(
+    name    = 'kiwi',
+    version = '0.1',
+    description = 'Python interface to the Kiwi codes.',
+    
+    packages = ['tunguska'],
+    
+    scripts = [ pjoin('scripts', x) for x in [
+        'kinherd_sourceview',
+        'kinherd_sourceview_pres', 
+        'gfdb_phaser',
+        'gfdb_downsample',
+        'autokiwi', 
+        'snufflek',
+    ]],
+    author = 'Sebastian Heimann',
+    author_email = 'sebastian.heimann@zmaw.de',
+    url = 'http://www.kinherd.org/',
+)
 
