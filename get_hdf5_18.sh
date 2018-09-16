cd62b83fc5c6c847bbdd29aafe0cb1f01c1bd122
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2018-09-16 17:35:01 +0200

    improved hdf5 compilation script

--- get_hdf5_18.sh
+++ get_hdf5_18.sh
@@ -17,12 +17,12 @@ tar -xzf "${hdf5}.tar.gz"
 cd "$hdf5"
 curdir=`pwd`
 ./configure --enable-fortran --prefix="$curdir/hdf5"
-make -j 2
+make -j 4
 make install
 
 echo "set these variables in Makefile.local:"
 echo
-echo "  HDF5DIR = hdf5-1.8.21/hdf5"
-echo "  INCHDF = -I$(HDF5DIR)/include"
-echo "  LIBHDF = $(HDF5DIR)/lib/libhdf5_fortran.a $(HDF5DIR)/lib/libhdf5.a -ldl -lz"
+echo "  HDF5DIR = ${hdf5}/hdf5"
+echo "  INCHDF = -I\$(HDF5DIR)/include"
+echo "  LIBHDF = \$(HDF5DIR)/lib/libhdf5_fortran.a \$(HDF5DIR)/lib/libhdf5.a -ldl -lz"
 echo
